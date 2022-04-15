06b7a014d98a317c71d32a8f82f9e23cda45e764
DESKTOP-KJ7MLQ3\User <goubin55@naver.com>
2022-04-15 16:58:31 +0900

    Test

---
+++ Cocktail/app/src/main/java/com/example/cocktail/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.cocktail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button start_button = (Button) findViewById(R.id.button_start);
+        start_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Testing1Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+}
---
+++ Cocktail/app/src/main/java/com/example/cocktail/Testing1Activity.java
@@ -0,0 +1,18 @@
+package com.example.cocktail;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Testing1Activity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_testing1);
+    }
+
+}
